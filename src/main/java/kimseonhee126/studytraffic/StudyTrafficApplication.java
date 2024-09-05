311f3f44ee1e4d5fefba39d58ba6d544cb238982
kimseonhee126 <kimseonhee126@gmail.com>
2024-09-05 15:11:18 +0900

    project settings

---
+++ src/main/java/kimseonhee126/studytraffic/StudyTrafficApplication.java
@@ -0,0 +1,13 @@
+package kimseonhee126.studytraffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTrafficApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTrafficApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimseonhee126/studytraffic/StudyTrafficApplicationTests.java
@@ -0,0 +1,13 @@
+package kimseonhee126.studytraffic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrafficApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
